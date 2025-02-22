a2eb6a4b2552af407cd5caeb36b42c948e268da6
takokan <takokan.dev@gmail.com>
2025-02-22 19:50:39 +0530

    added single treated ws

--- single-threaded/src/main.rs
+++ single-threaded/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    env::set_current_dir, io::{prelude, BufRead, BufReader}, net::{TcpListener, TcpStream}
+};
+
+fn main(){
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming(){
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream){
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+    .lines()
+    .map(|result| result.unwrap())
+    .take_while(|line| !line.is_empty())
+    .collect();
+
+    println!("Request: http_request {:?}", http_request);
 }
